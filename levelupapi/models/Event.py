15ce0b658aeff77f868bc95be0c6fc638f473210
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-14 10:03:43 -0600

    fixtures and data added

--- levelupapi/models/Event.py
+++ levelupapi/models/Event.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True)
     organizer = models.ForeignKey(Gamer, on_delete=models.SET_NULL, null=True)
     description = models.CharField(max_length=200)
-    date = models.DateField(auto_now=False)
-    time = models.TimeField(auto_now=False)
+    date = models.DateField(auto_now=False, null=True)
+    time = models.TimeField(auto_now=False, null=True)
     joined = models.BooleanField(None)
     
