dbc4fd2bfa4b4ede6a2a7e0813ae0cdf7ee252db
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-14 11:24:10 -0600

    authentication complete

--- levelup/urls.py
+++ levelup/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from levelupapi.views import register_user, login_user
 
 urlpatterns = [
+    # Requests to http://localhost:8000/register will be routed to the register_user function
+    path('register', register_user),
+    # Requests to http://localhost:8000/login will be routed to the login_user function
+    path('login', login_user),
     path('admin/', admin.site.urls),
 ]
