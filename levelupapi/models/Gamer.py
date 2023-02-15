a93304f8c868aceb42b25cbd3e9bc119f4f0aa48
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-15 13:05:14 -0600

    added depth to my data

--- levelupapi/models/Gamer.py
+++ levelupapi/models/Gamer.py
@@ -6,3 +6,7 @@ class Gamer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=50)
+
+    @property
+    def full_name(self):
+        return f'{self.user.first_name} {self.user.last_name}'
